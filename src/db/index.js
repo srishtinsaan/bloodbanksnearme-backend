96b54c20676d5ec0e2be0e260871c2e826787d35
Srishti Singh <srish17816@gmail.com>
2026-07-20 16:00:09 +0530

    errors resolved completely

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,40 @@
-import dotenv from "dotenv";
-dotenv.config({ path: "../.env" });
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
 
-import { app } from "../app.js";
+let cached = global.mongooseConnection;
 
-export default app;
+if (!cached) {
+  cached = global.mongooseConnection = { conn: null, promise: null };
+}
+
+const connectDB = async () => {
+  if (cached.conn) {
+    console.log("MongoDB: using cached connection");
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(`${process.env.MONGODB_URI}/${DB_NAME}`, {
+        bufferCommands: false,
+      })
+      .then((mongooseInstance) => {
+        console.log(
+          `\n MongoDB connected !! DB HOST : ${mongooseInstance.connection.host}`
+        );
+        return mongooseInstance;
+      });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (error) {
+    cached.promise = null;
+    console.log("MongoDB connection error ::", error);
+    throw error;
+  }
+
+  return cached.conn;
+};
+
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -3,30 +3,4 @@ dotenv.config({ path: "../.env" });
 
 import { app } from "./app.js";
 
-// DATABASE
-import connectDB from "./db/index.js";
-
-
-// Keep track of connections (important for serverless)
-let isMongoConnected = false;
-
-async function initConnections() {
-  try {
-    // MongoDB
-    if (!isMongoConnected) {
-      await connectDB();
-      isMongoConnected = true;
-      console.log("MongoDB connected ✅");
-    }
-
-    
-
-  } catch (error) {
-    console.error("Connection FAILED ❌", error);
-  }
-}
-
-// Initialize once (cold start safe)
-initConnections();
-
 export default app;
